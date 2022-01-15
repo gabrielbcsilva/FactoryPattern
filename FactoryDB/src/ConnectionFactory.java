fda638e7d7b5f7279017433ff1d356dc7b4dd131
Gabriel Silva <95655030+gabriel-silva-kothe@users.noreply.github.com>
2022-01-14 21:37:30 -0300

    Modificando classe para Conceito de pool de conexões
    O pool de conexões gerencia a quantidade de conexões simultaneas em um banco de dados

--- FactoryDB/src/ConnectionFactory.java
+++ FactoryDB/src/ConnectionFactory.java
@@ -1,11 +1,28 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+
 String dbURL = "jdbc:mysql://localhost/database?userTimezone=true&serverTimezone=UTC";
+DataSource dataSource;
+
+
+  public ConnectionFactory() {
+	  ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+	  comboPooledDataSource.setJdbcUrl(dbURL);
+	  comboPooledDataSource.setUser("root");
+	  comboPooledDataSource.setPassword("");
+	  
+	  comboPooledDataSource.setMaxPoolSize(15);
+	  
+	  this.dataSource = comboPooledDataSource;
+  }
 
 public Connection returnConnection() throws SQLException{
-	return DriverManager.getConnection(dbURL, "",""); //nome e senha 
+	return this.dataSource.getConnection(); //nome e senha 
 }
 }
